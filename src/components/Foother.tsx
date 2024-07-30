4fc7207ff481d6c1854ca4908b1b267421a5c91c
elarbi <arbilamfarrad@gmail.com>
2024-07-30 13:26:24 +0100

    fix error navbar

--- src/components/Foother.tsx
+++ src/components/Foother.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Foother = () => {
   return (
-    <footer className="bg-white">
+    <footer className="bg-white border-t-2 mt-5">
       <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
           <div>
