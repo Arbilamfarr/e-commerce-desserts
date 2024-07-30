a20b179985556a4274cbabdb6c563c3f9f22d19c
elarbi <arbilamfarrad@gmail.com>
2024-07-30 14:50:18 +0100

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-white shadow-sm mb-4">
+    <header className="bg-white shadow-sm">
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="md:flex md:items-center md:gap-12">
